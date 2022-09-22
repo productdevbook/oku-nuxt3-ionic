fff99d8b04e2316871182fde7177792d3c088c73
Mehmet <hi@productdevbook.com>
2022-09-22 16:45:02 +0300

    feat: color mode, tailwindcss ... (#14)
    * feat: install ionic
    
    * fix: ionic
    
    * fix: general
    
    * Update ci.yml

---
+++ src/pages/tabs/tab2.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  import {actionSheetController} from "@ionic/vue"
+  import {camera, close, trash} from "ionicons/icons"
+  const colorMode = useColorMode()
+</script>
+
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <div>
+        <h1>Color mode: {{ $colorMode.value }}</h1>
+        <select v-model="$colorMode.preference">
+          <option value="system">System</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          <option value="sepia">Sepia</option>
+        </select>
+      </div>
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    </ion-content>
+  </ion-page>
+</template>
